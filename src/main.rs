27fff097f0900cc9d44a8b828a5d876b3e3725c5
Giacomo Rizzi <gufoes@gmail.com>
2023-01-21 16:14:03 +0100

    maybe upgrade dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use rayon::prelude::*;
-use ggez::{Context, ContextBuilder, GameResult};
 use ggez::event::{self, EventHandler};
 use ggez::graphics;
-use latex::Latex2D;
-use ggez::input::keyboard::KeyMods;
 use ggez::input::keyboard::KeyCode;
+use ggez::input::keyboard::KeyMods;
+use ggez::{Context, ContextBuilder, GameResult};
+use latex::Latex2D;
+use rayon::prelude::*;
 
-mod vec;
 mod ag;
-mod utils;
 mod latex;
+mod utils;
+mod vec;
 
-const AGENT_NUM: usize = 4000;
+const AGENT_NUM: usize = 10000;
 const ST_LEN: usize = 40;
 const BRUSH_SIZE: f32 = 50.0;
 macro_rules! map(
@@ -30,17 +30,16 @@ fn main() {
     // rayon::ThreadPoolBuilder::new().num_threads(12).build_global().expect("no thread pool");
 
     // Make a Context and an EventLoop.
-    let (mut ctx, mut event_loop) =
-       ContextBuilder::new("game_name", "author_name")
-       .window_setup(ggez::conf::WindowSetup {
+    let (mut ctx, mut event_loop) = ContextBuilder::new("game_name", "author_name")
+        .window_setup(ggez::conf::WindowSetup {
             title: "An easy, good game".to_owned(),
             samples: ggez::conf::NumSamples::Zero,
             vsync: true,
             icon: "".to_owned(),
             srgb: true,
         })
-       .window_mode(ggez::conf::WindowMode {
-            width: 800.0,
+        .window_mode(ggez::conf::WindowMode {
+            width: 1000.0,
             height: 800.0,
             maximized: false,
             fullscreen_type: ggez::conf::FullscreenType::Windowed,
@@ -51,8 +50,8 @@ fn main() {
             max_height: 0.0,
             resizable: false,
         })
-           .build()
-           .expect("cannot make window");
+        .build()
+        .expect("cannot make window");
 
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object
@@ -62,7 +61,7 @@ fn main() {
     // Run!
     match event::run(&mut ctx, &mut event_loop, &mut my_game) {
         Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e)
+        Err(e) => println!("Error occured: {}", e),
     }
 }
 
@@ -89,7 +88,7 @@ impl MyGame {
     pub fn new(ctx: &mut Context) -> MyGame {
         use rand::Rng;
 
-            // graphics::set_fullscreen(ctx, ggez::conf::FullscreenType::True).unwrap();
+        // graphics::set_fullscreen(ctx, ggez::conf::FullscreenType::True).unwrap();
 
         // Load/create resources here: images, fonts, sounds, etc.
         let mut agents = vec![];
@@ -98,12 +97,12 @@ impl MyGame {
             agents.push(ag::Agent::new(
                 agents.len(),
                 vec::Vec {
-                    x: rand::thread_rng().gen_range(0.0, w),
-                    y: rand::thread_rng().gen_range(0.0, h),
+                    x: rand::thread_rng().gen_range(0.0..w),
+                    y: rand::thread_rng().gen_range(0.0..h),
                 },
                 vec::Vec {
-                    x: 0.0,//rand::thread_rng().gen_range(-1.0, 1.0),
-                    y: 0.0,//rand::thread_rng().gen_range(-1.0, 1.0),
+                    x: 0.0, //rand::thread_rng().gen_range(-1.0, 1.0),
+                    y: 0.0, //rand::thread_rng().gen_range(-1.0, 1.0),
                 },
             ))
         }
@@ -158,7 +157,7 @@ impl MyGame {
             // Compare
             println!("ld {}: {:.4}", ld, t_diff);
             if !min.is_none() && min.unwrap().0 < t_diff {
-                break
+                break;
             }
             if min.is_none() || min.unwrap().0 > t_diff {
                 min = Some((t_diff, ld as f32));
@@ -173,7 +172,9 @@ impl MyGame {
     pub fn update_latex(&mut self, w: f32, h: f32) {
         let mut latex = Latex2D::new(w / 2.0 / self.latex_div, w, h);
         let _t0 = utils::now();
-        self.agents.iter().for_each(|x| latex.add((x.pos.x, x.pos.y), x.clone()));
+        self.agents
+            .iter()
+            .for_each(|x| latex.add((x.pos.x, x.pos.y), x.clone()));
         // println!("latex:   {:.3}", utils::now() - _t0);
         self.latex = latex
     }
@@ -184,7 +185,7 @@ impl EventHandler for MyGame {
         let (w, h) = graphics::drawable_size(ctx);
         let pos = ggez::input::mouse::position(ctx);
 
-        for _ in 0..(self.fast*2).max(1) {
+        for _ in 0..(self.fast * 2).max(1) {
             let mut tim = utils::Timer::new("UPDATE");
             self.update_latex(w, h);
             tim.tick("latex updated");
@@ -198,23 +199,18 @@ impl EventHandler for MyGame {
                 agents: &self.latex,
                 gravity_f: self.gravity_f,
                 gravity: match self.gravity_mod {
-                    1 => vec![
-                        vec::Vec::new_from(w*0.5, h*0.5),
-                    ],
+                    1 => vec![vec::Vec::new_from(w * 0.5, h * 0.5)],
                     2 => vec![
-                        vec::Vec::new_from(w*0.5, h*0.5),
-                        vec::Vec::new_from(w*(0.5 + _dx), h*(0.5 + _dy)),
+                        vec::Vec::new_from(w * 0.5, h * 0.5),
+                        vec::Vec::new_from(w * (0.5 + _dx), h * (0.5 + _dy)),
                     ],
                     3 => vec![
-                        vec::Vec::new_from(w*0.5, h*0.5),
+                        vec::Vec::new_from(w * 0.5, h * 0.5),
                         vec::Vec::new_from(pos.x, pos.y),
                     ],
-                    4 => vec![
-                        vec::Vec::new_from(pos.x, pos.y),
-                    ],
-                    _ => vec![]
-
-                 },
+                    4 => vec![vec::Vec::new_from(pos.x, pos.y)],
+                    _ => vec![],
+                },
             };
 
             self.agents.par_iter_mut().for_each(|x| x.update(&update));
@@ -240,20 +236,26 @@ impl EventHandler for MyGame {
 
         // Draw bbackground
         let mut mb_bg = &mut graphics::MeshBuilder::new();
-        mb_bg.rectangle(graphics::DrawMode::fill(), graphics::Rect::new(0.0, 0.0, w, h),
-                graphics::Color::new(0.0, 0.0, 0.0, 0.97));
+        mb_bg.rectangle(
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(0.0, 0.0, w, h),
+            graphics::Color::new(0.0, 0.0, 0.0, 0.97),
+        );
 
         // Get stats
-        let max_speed: f32 = self.agents.par_iter()
+        let max_speed: f32 = self
+            .agents
+            .par_iter()
             .fold(|| 0.0, |v: f32, x| v.max(x.s_vel))
             .reduce(|| 0.0, |v: f32, x| v.max(x));
-        let mut col: [f32; 3] = self.agents.par_iter()
+        let mut col: [f32; 3] = self
+            .agents
+            .par_iter()
             .fold(|| [0.0, 0.0, 0.0], |v, x| utils::sum(&v, &x.color))
             .reduce(|| [0.0, 0.0, 0.0], |v, x| utils::sum(&v, &x));
         utils::softmax_fast(&mut col);
 
         tim.tick("done stats done");
-
 
         let mut stats_mesh = ggez::graphics::MeshBuilder::new();
         let mut tot = 0.0;
@@ -262,42 +264,45 @@ impl EventHandler for MyGame {
         for i in 0..3 {
             stats_mesh.rectangle(
                 graphics::DrawMode::fill(),
-                ggez::graphics::Rect::new(10.0 + tot * width, 10.0,
-                    col[i] * width, height),
-                    graphics::Color::new(
-                        if i == 0 { 1.0 } else { 0.0 },
-                        if i == 1 { 1.0 } else { 0.0 },
-                        if i == 2 { 1.0 } else { 0.0 },
-                        1.0
-                    )
+                ggez::graphics::Rect::new(10.0 + tot * width, 10.0, col[i] * width, height),
+                graphics::Color::new(
+                    if i == 0 { 1.0 } else { 0.0 },
+                    if i == 1 { 1.0 } else { 0.0 },
+                    if i == 2 { 1.0 } else { 0.0 },
+                    1.0,
+                ),
             );
-            tot+= col[i];
+            tot += col[i];
         }
-
 
         tim.tick("draw stats done");
 
         self.avg_stats_vel.push(max_speed);
-        if self.avg_stats_vel.len() > ST_LEN { self.avg_stats_vel.remove(0); }
+        if self.avg_stats_vel.len() > ST_LEN {
+            self.avg_stats_vel.remove(0);
+        }
         let max_speed = utils::avg(&self.avg_stats_vel);
 
-        let max_range: f32 = self.agents.par_iter()
+        let max_range: f32 = self
+            .agents
+            .par_iter()
             .fold(|| 0.0, |v: f32, x| v.max(x.s_in_range as f32))
             .reduce(|| 0.0, |v: f32, x| v.max(x));
         self.avg_stats_range.push(max_range as f32);
-        if self.avg_stats_range.len() > ST_LEN { self.avg_stats_range.remove(0); }
+        if self.avg_stats_range.len() > ST_LEN {
+            self.avg_stats_range.remove(0);
+        }
         let max_range = utils::avg(&self.avg_stats_range);
 
         tim.tick("drew stats");
 
-
         // Draw agents
         let _t0 = utils::now();
         let mut mb = &mut graphics::MeshBuilder::new();
-        self.agents.iter().for_each(|x| x.draw(ctx, &mut mb, &mut mb_bg, max_speed, max_range));
+        self.agents
+            .iter()
+            .for_each(|x| x.draw(ctx, &mut mb, &mut mb_bg, max_speed, max_range));
         tim.tick("drew agents");
-
-
 
         // Draw background and foreground
         let mb_bg = mb_bg.build(ctx).unwrap();
@@ -314,42 +319,45 @@ impl EventHandler for MyGame {
         // let _t0 = utils::now();
         tim.tick("drawed bg and fg");
 
-
         graphics::present(ctx)?;
         tim.tick("presented");
         // println!("present:    {:.3}", utils::now() - _t0);
 
         print!("{}[2J", 27 as char);
-        println!("FPS:  DRAW = {:.2}  UPDATE = {:.2}", ggez::timer::fps(ctx), self.get_fps());
+        println!(
+            "FPS:  DRAW = {:.2}  UPDATE = {:.2}",
+            ggez::timer::fps(ctx),
+            self.get_fps()
+        );
         tim.show();
         if utils::now() > self.frames_start + 1.0 {
             self.restart_fps();
         }
         Ok(())
     }
-    fn mouse_motion_event(
-        &mut self,
-        _ctx: &mut Context,
-        x: f32,
-        y: f32,
-        dx: f32,
-        dy: f32
-    ) {
+    fn mouse_motion_event(&mut self, _ctx: &mut Context, x: f32, y: f32, dx: f32, dy: f32) {
         let p = vec::Vec::new_from(x, y);
         let d = vec::Vec::new_from(dx, dy);
         let radius = BRUSH_SIZE;
         if self.btn_left {
-            let agents: Vec<usize> = self.latex.get((x, y), radius).iter().map(|x| x.id).collect();
+            let agents: Vec<usize> = self
+                .latex
+                .get((x, y), radius)
+                .iter()
+                .map(|x| x.id)
+                .collect();
             // let mut i: Vec<usize> = Vec::new();
             // let mut ids = std::collections::HashSet::new();
             agents.iter().for_each(|id| {
                 // ids.insert(id);
                 let x = self.agents.get_mut(*id).expect("element in latex too much");
                 let dist = x.pos.dist_mod(&p, self.latex.w, self.latex.h);
-                if dist > radius { return; }
+                if dist > radius {
+                    return;
+                }
                 let mut d = d.clone();
                 // d.mul(2.0);
-                d.mul(( 1.0 - dist / radius) * 0.1);
+                d.mul((1.0 - dist / radius) * 0.1);
                 x.vel.add(&d);
                 // x.pos.add(&d);
             });
@@ -362,7 +370,7 @@ impl EventHandler for MyGame {
         _ctx: &mut Context,
         _button: ggez::input::mouse::MouseButton,
         _x: f32,
-        _y: f32
+        _y: f32,
     ) {
         use ggez::input::mouse::MouseButton as mb;
         match _button {
@@ -371,8 +379,8 @@ impl EventHandler for MyGame {
             mb::Middle => {
                 self.btn_middle = true;
                 self.adjust_latex_div(_ctx);
-            },
-            mb::Other(_) => {},
+            }
+            mb::Other(_) => {}
         }
     }
     fn mouse_button_up_event(
@@ -380,23 +388,22 @@ impl EventHandler for MyGame {
         _ctx: &mut Context,
         _button: ggez::input::mouse::MouseButton,
         _x: f32,
-        _y: f32
+        _y: f32,
     ) {
         use ggez::input::mouse::MouseButton as mb;
         match _button {
             mb::Left => self.btn_left = false,
             mb::Right => self.btn_right = false,
             mb::Middle => self.btn_middle = false,
-            mb::Other(_) => {},
+            mb::Other(_) => {}
         }
     }
     fn mouse_wheel_event(&mut self, _ctx: &mut Context, _x: f32, _y: f32) {
         self.agents.par_iter_mut().for_each(|x| {
-            x.pos_w+= _y;
+            x.pos_w += _y;
         });
         println!("pos_w set to {}", self.agents[0].pos_w);
     }
-
 
     fn key_down_event(&mut self, _ctx: &mut Context, key: KeyCode, mods: KeyMods, _: bool) {
         let rounds = if mods.contains(KeyMods::SHIFT | KeyMods::CTRL) {
@@ -405,20 +412,20 @@ impl EventHandler for MyGame {
             100
         };
         match key {
-            KeyCode::F |
-            KeyCode::A |
-            KeyCode::R |
-            KeyCode::B |
-            KeyCode::G |
-            KeyCode::Z |
-            KeyCode::X |
-            KeyCode::Escape => {
+            KeyCode::F
+            | KeyCode::A
+            | KeyCode::R
+            | KeyCode::B
+            | KeyCode::G
+            | KeyCode::Z
+            | KeyCode::X
+            | KeyCode::Escape => {
                 self.key_mod = key;
-            },
-            _  => { }
+            }
+            _ => {}
         }
 
-        let input_num = map!{
+        let input_num = map! {
             KeyCode::Key1 => 1,
             KeyCode::Key2 => 2,
             KeyCode::Key3 => 3,
@@ -445,7 +452,6 @@ impl EventHandler for MyGame {
                     let s = self.agents.len();
                     self.agents.get_mut(utils::rand_usize(s)).unwrap().color = [1.0, 0.0, 0.0];
                 }
-
             }
             KeyCode::R => {
                 println!("making one aggressive");
@@ -453,7 +459,6 @@ impl EventHandler for MyGame {
                     let s = self.agents.len();
                     self.agents.get_mut(utils::rand_usize(s)).unwrap().color = [1.0, 0.0, 0.0];
                 }
-
             }
             KeyCode::B => {
                 println!("making one aggressive");
@@ -461,7 +466,6 @@ impl EventHandler for MyGame {
                     let s = self.agents.len();
                     self.agents.get_mut(utils::rand_usize(s)).unwrap().color = [0.0, 0.0, 1.0];
                 }
-
             }
             KeyCode::G => {
                 println!("making one green");
@@ -469,14 +473,13 @@ impl EventHandler for MyGame {
                     let s = self.agents.len();
                     self.agents.get_mut(utils::rand_usize(s)).unwrap().color = [0.0, 1.0, 0.0];
                 }
-
             }
             KeyCode::F => {
                 println!("making fast");
                 match input_num {
                     Some(f) => {
                         self.fast = *f;
-                    },
+                    }
                     _ => {}
                 }
             }
@@ -485,7 +488,7 @@ impl EventHandler for MyGame {
                 match input_num {
                     Some(f) => {
                         self.gravity_mod = *f;
-                    },
+                    }
                     _ => {}
                 }
             }
@@ -494,7 +497,7 @@ impl EventHandler for MyGame {
                 match input_num {
                     Some(f) => {
                         self.gravity_f = *f as f32;
-                    },
+                    }
                     _ => {}
                 }
             }
@@ -503,27 +506,26 @@ impl EventHandler for MyGame {
     }
 }
 
-
 #[test]
-fn test_vec () {
+fn test_vec() {
     let w = 10.0;
     let h = 10.0;
     let tol = 0.001;
 
-    let v1 = vec::Vec { x: w-1.0, y: 0.0 };
-    let v2 = vec::Vec { x: w-2.0, y: 0.0 };
+    let v1 = vec::Vec { x: w - 1.0, y: 0.0 };
+    let v2 = vec::Vec { x: w - 2.0, y: 0.0 };
     let rel = v1.rel(&v2, w, h);
     println!("{:?}", rel);
     assert!((rel.dist(&v1) - 1.0).abs() < tol);
 
-    let v1 = vec::Vec { x: w-1.0, y: 0.0 };
+    let v1 = vec::Vec { x: w - 1.0, y: 0.0 };
     let v2 = vec::Vec { x: 1.0, y: 0.0 };
     let rel = v1.rel(&v2, w, h);
     println!("{:?}", rel);
     assert!((rel.dist(&v1) - 2.0).abs() < tol);
 
     let v1 = vec::Vec { x: 1.0, y: 0.0 };
-    let v2 = vec::Vec { x: w-1.0, y: 0.0 };
+    let v2 = vec::Vec { x: w - 1.0, y: 0.0 };
     let rel = v1.rel(&v2, w, h);
     println!("{:?}", rel);
     assert!((rel.dist(&v1) - 2.0).abs() < tol);
@@ -536,7 +538,7 @@ fn test_vec () {
 }
 
 #[test]
-fn test_latex () {
+fn test_latex() {
     let res = 10.0;
     let w = 100.0;
     let h = 100.0;
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,3 @@
-
 #[allow(dead_code)]
 pub struct Timer {
     times: Vec<(String, f64)>,
@@ -7,9 +6,7 @@ pub struct Timer {
 #[allow(dead_code)]
 impl Timer {
     pub fn new(label: &str) -> Self {
-        let mut x = Self {
-            times: vec![],
-        };
+        let mut x = Self { times: vec![] };
         x.tick(label);
         x
     }
@@ -18,87 +15,94 @@ impl Timer {
     }
     pub fn tick(&mut self, label: &str) -> (String, f64) {
         self.times.push((label.to_string(), now()));
-        self.times.get(self.times.len()-2).unwrap_or(&("start".to_string(), 0.0)).clone()
+        self.times
+            .get(self.times.len() - 2)
+            .unwrap_or(&("start".to_string(), 0.0))
+            .clone()
     }
     pub fn diff_or_0(&self, a: usize, b: usize) -> f64 {
         let a = self.times.get(a).unwrap_or(&("start".to_string(), 0.0)).1;
         let b = self.times.get(b).unwrap_or(&("start".to_string(), a)).1;
-        a-b
+        a - b
     }
     pub fn show(&self) {
         for i in 0..self.times.len() {
-            println!("{:>30}: {:.9}", self.times[i].0, self.diff_or_0(i, i-1));
+            println!("{:>30}: {:.9}", self.times[i].0, self.diff_or_0(i, i - 1));
         }
-        println!("{:>30}: {:.9}", "total", self.diff_or_0(self.times.len()-1, 0));
+        println!(
+            "{:>30}: {:.9}",
+            "total",
+            self.diff_or_0(self.times.len() - 1, 0)
+        );
     }
 }
-
 
 #[allow(dead_code)]
 pub fn rand_int(max: u32) -> u32 {
     use rand::Rng;
-    rand::thread_rng().gen_range(0, max)
+    rand::thread_rng().gen_range(0..max)
 }
 #[allow(dead_code)]
 pub fn rand_usize(max: usize) -> usize {
     use rand::Rng;
-    rand::thread_rng().gen_range(0, max)
+    rand::thread_rng().gen_range(0..max)
 }
 #[allow(dead_code)]
 pub fn rand_intr(min: i32, max: i32) -> i32 {
     use rand::Rng;
-    rand::thread_rng().gen_range(min, max)
+    rand::thread_rng().gen_range(min..max)
 }
 #[allow(dead_code)]
 pub fn rand_float(min: f32, max: f32) -> f32 {
     use rand::Rng;
-    rand::thread_rng().gen_range(min, max)
+    rand::thread_rng().gen_range(min..max)
 }
 #[allow(dead_code)]
-pub fn maybe(pty: f32) -> bool{
+pub fn maybe(pty: f32) -> bool {
     rand_float(0., 1.) < pty
 }
 
 #[allow(dead_code)]
 pub fn now() -> f64 {
     use std::time::{SystemTime, UNIX_EPOCH};
-    let ste = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let ste = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
     ste.as_secs() as f64 + ste.subsec_micros() as f64 / 1_000_000.0
 }
-
 
 pub fn avg(numbers: &[f32]) -> f32 {
     numbers.iter().sum::<f32>() / numbers.len() as f32
 }
 pub fn vavg(a: &mut [f32], b: &[f32], x: f32) {
     for i in 0..a.len() {
-        a[i]+= b[i] * x;
-        a[i]/= 1.0 + x;
+        a[i] += b[i] * x;
+        a[i] /= 1.0 + x;
     }
 }
 pub fn sum(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
-    [
-        a[0] + b[0],
-        a[1] + b[1],
-        a[2] + b[2],
-    ]
+    [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 }
 pub fn eavg(a: &mut f32, b: f32, x: f32) {
-        *a+= b * x;
-        *a/= 1.0 + x;
+    *a += b * x;
+    *a /= 1.0 + x;
 }
 
 pub fn softmax(v: &mut [f32]) {
     norm(v);
-    let sum = v.iter().fold(0.0, |sum, &val| sum+val);
+    let sum = v.iter().fold(0.0, |sum, &val| sum + val);
     if sum > 0. {
-        for n in v { *n /= sum }
+        for n in v {
+            *n /= sum
+        }
     }
 }
 pub fn softmax_fast(v: &mut [f32]) {
-    let sum = v.iter().fold(0.0, |sum, &val| sum+val);
+    let sum = v.iter().fold(0.0, |sum, &val| sum + val);
     if sum > 0. {
-        for n in v { *n /= sum }
+        for n in v {
+            *n /= sum
+        }
     }
 }
 
@@ -113,13 +117,13 @@ pub fn norm(v: &mut [f32]) {
             min = *i;
         }
     }
-    let mut n = max-min;
+    let mut n = max - min;
     if n == 0. {
         n = 1.;
     }
     for i in v.iter_mut() {
-        *i-= min;
-        *i/= n;
+        *i -= min;
+        *i /= n;
     }
 }
 pub fn norm_2(v: &mut [f32]) {
@@ -133,11 +137,11 @@ pub fn norm_2(v: &mut [f32]) {
             min = *i;
         }
     }
-    let mut n = max-min;
+    let mut n = max - min;
     if n == 0. {
         n = 1.;
     }
     for i in v.iter_mut() {
-        *i/= n;
+        *i /= n;
     }
 }
